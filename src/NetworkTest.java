b1f6057112310a469b817340206c98927df1ca4a
LS2991 <louisbsantiago@gmail.com>
2015-03-01 16:37:10 -0500

    undo booboo

---
+++ src/NetworkTest.java
@@ -0,0 +1,128 @@
+import static org.junit.Assert.*;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class NetworkTest {
+	
+	
+	@Test
+	public void fileTransferTest() throws IOException { //includes send and receiving a file from both client and server side
+		final FileOutputStream fOStream = null;
+		final BufferedOutputStream bOStream = null;
+		ServerSocket s = null;
+		final Socket incoming = null;
+		//Socket clientS = null;
+		final FileInputStream fIStream = null;
+		final BufferedInputStream bIStream = null;
+		final OutputStream oStream = null;
+		
+		
+		
+<<<<<<< HEAD
+//		Thread serverThread = new Thread() {
+//			public void run() {
+//				try {
+//					File f = new File("//192.168.1.229/Users/Public/ServerSide/database.txt");
+//					NetworkConnection.sendFile(fIStream, bIStream, oStream, incoming, f);
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} 
+//			}
+//		};
+//		
+//		serverThread.start();
+		
+		NetworkClient.getFile(fOStream, bOStream, incoming);
+		
+//		serverThread = new Thread() {
+//			public void run() {
+//				try {
+//					NetworkConnection.recieveFile(fOStream, bOStream, incoming);
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} 
+//			}
+//		};
+//		
+//		serverThread.start();
+=======
+	/*	Thread serverThread = new Thread() {
+			public void run() {
+				try {
+					File f = new File("//192.168.1.229/Users/Public/ServerSide/database.txt");
+					NetworkConnection.sendFile(fIStream, bIStream, oStream, incoming, f);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+			}
+		};
+		
+		serverThread.start(); */
+		
+		NetworkClient.getFile(fOStream, bOStream, incoming);
+		
+	/*	Thread serverThread = new Thread() {
+			public void run() {
+				try {
+					NetworkConnection.recieveFile(fOStream, bOStream, incoming);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+			}
+		};
+		
+		serverThread.start();*/
+>>>>>>> origin/master
+		
+		File f = new File("//192.168.1.229/Users/Public/ClientSide/databaseR.txt");
+	//	NetworkClient.sendFile(fIStream, bIStream, oStream, f);
+		//assertTrue(s.isBound());
+		//assertTrue(s.isClosed());
+		
+		
+		try {
+			Thread.sleep(2000); //waits for files to appear
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertTrue(f.exists());
+		
+<<<<<<< HEAD
+//		File ff = new File("//192.168.1.229/Users/Public/ServerSide/databaseRR.txt");
+//		
+//		assertTrue(ff.exists());
+=======
+		//File ff = new File("//192.168.1.229/Users/Public/ServerSide/databaseRR.txt");
+		
+		//assertTrue(ff.exists());
+>>>>>>> origin/master
+	}
+	
+//	@Test
+//	public void savedFileExistsTest() throws IOException {
+//		File f = new File("C:/Users/Louis/Desktop/databaseRR.txt");
+//		assertTrue(f.exists());
+//	}
+
+}
