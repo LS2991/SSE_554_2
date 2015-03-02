393604f68fa50463016d5de36dd52f09a0263e20
KeithRuss <keith.m.russ@gmail.com>
2015-03-02 18:02:19 -0500

    Adding My Unit Tests

--- src/Tests.java
+++ src/Tests.java
@@ -5,20 +5,64 @@ import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
 
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class Tests {
+	private static final File File = null;
 	
+	@Test
+	public void SaveGameTest() throws FileNotFoundException
+	{
+		File f = new File("save_files");
+		DatabaseStuff.SaveGame(1,f);
+		File file = new File("save001.txt");
+		try {
+			Scanner input = new Scanner(file);
+			while (input.hasNextLine())
+			{
+				String line = input.nextLine();
+				assertEquals("The game was saved.",line);
+			}
+			input.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.print("File not found");
+			}
+	}
+	
+	@Test
+	public void LoadGameTest() throws FileNotFoundException
+	{
+		File f = new File("save_files");
+		String name = DatabaseStuff.LoadGame(1, f);
+		assertEquals("Keith Russ",name);
+	}
+	
+	@Test
+	public void UpdateFileTest() throws FileNotFoundException
+	{
+		File f = new File("save_files");
+		DatabaseStuff.UpdateFile(1, "Keith Russel", f);
+		String name = DatabaseStuff.LoadGame(1, f);
+		assertEquals("Keith Russel",name);
+	}
 	
 	@Test
 	public void fileTransferTest() throws IOException { //includes send and receiving a file from both client and server side
